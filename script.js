3be55c025f58d2c5026b97db59be96da60ef2543
Probytech <tolikshift@gmail.com>
2026-04-21 11:43:29 +0300

    Add burger menu animation and move images to folder

---
+++ script.js
@@ -0,0 +1,6 @@
+const burgerMenu = document.querySelector('#burger-menu');
+const header = document.querySelector('header');
+
+burgerMenu.addEventListener('click', function() {
+    header.classList.toggle('active');
+})
