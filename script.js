960bdd9a576ff1afc01a207e1d0e77198cf49a7e
Probytech <tolikshift@gmail.com>
2026-04-21 14:50:14 +0300

    Remove header active on menu item click

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const header = document.querySelector('header');
 burgerMenu.addEventListener('click', function() {
     header.classList.toggle('active');
 })
+
+/* MENU ITEMS */
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(menuItem => {
+    menuItem.addEventListener('click', function () {
+        header.classList.remove('active');
+    })
+});
